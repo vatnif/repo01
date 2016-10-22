7098024efa461f7e0205aceefc7f37da5bdfea1c
vatnif <vatnif>
2016-10-22 12:37:01 +0300

    Assignmet 2 completed.

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -3,11 +3,53 @@
 
 angular.module('ShoppingListCheckOff',[])
 .controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController);
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-ToBuyController.$inject = ['$scope'];
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService){
+  var toBuy = this;
 
-function ToBuyController($scope){
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  toBuy.buyItem = function(itemIndex){
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  }
+}
+
+AlreadyBoughtController.$inject =['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService){
+  var bought = this;
+
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService(){
+  var service = this;
+
+  var toBuyItems = [
+    { name: "cookies", quantity: 10 },
+    { name: "apples", quantity: 15 },
+    { name: "oranges", quantity: 12 },
+    { name: "bananas", quantity: 30 },
+    { name: "pears", quantity: 20 },
+  ];
+
+  var boughtItems = [];
+
+  service.buyItem = function(itemIndex){
+    boughtItems.push(toBuyItems[itemIndex]);
+    toBuyItems.splice(itemIndex, 1);
+  }
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
 
 }
 
